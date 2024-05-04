7ff9ad2ee57372930282d18038de7056d9772e81
Kszinhu@POP_os <rodrigues.cassiano2007@gmail.com>
2024-05-04 16:00:00 -0300

    build: add esmExternal if defined?

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import withPlaiceholder from '@plaiceholder/next';
 
 import i18nConfig from './next-i18next.config.js';
@@ -11,7 +12,7 @@ const nextConfig = {
   i18n,
   reactStrictMode: true,
   experimental: {
-    esmExternals,
+    ...(esmExternals && { esmExternals }),
   },
   typescript: {
     tsconfigPath,
